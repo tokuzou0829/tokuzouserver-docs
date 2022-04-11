510e2eed96eae920b7f98219665862ab7b7f2020
とくぞう <77785208+tokuzou0829@users.noreply.github.com>
2022-04-11 21:39:58 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/tokuzou0829/tokuzouserver-docs/edit/main/docs',
+          editUrl: 'https://github.com/tokuzou0829/tokuzouserver-docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tokuzou0829/tokuzouserver-docs/edit/main/blog',
+            'https://github.com/tokuzou0829/tokuzouserver-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
